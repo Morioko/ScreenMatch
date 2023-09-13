c2bf4c69482933c05a5656570479c3c548c64e33
Morioko <cristiano.sushy@hotmail.com>
2023-09-13 16:02:42 -0300

    Initial commit

---
+++ src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
@@ -0,0 +1,4 @@
+package br.com.alura.screenmatch.calculos;
+
+public class FiltroDeRecomendacao {
+}
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,32 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+        this.totalDeAvaliacoes = totalDeAvaliacoes;
+    }
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pedaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+
+}
